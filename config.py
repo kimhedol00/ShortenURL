460615cf969c34c812bc1873d490f41e38fe8945
kimhedol00 <hedol00@g.skku.edu>
2023-05-20 14:18:10 +0900

    add feature short url

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-host = "localhost:8000"
+import os
+
+host = os.environ['HOST']
+redis_host = os.environ['REDIS_HOST']
+redis_port = os.environ['REDIS_PORT']
+
+export HOST="localhost:8080"
+export REDIS_HOST="localhost"
+export REDIS_PORT=6379
+
+pip install redis
